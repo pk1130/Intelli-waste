97870214c4f5097f850f973a5e377b0ed172a0a7
pk1130 <pk1130@umd.edu>
2021-03-07 22:05:48 +0300

    added model prediction to Flask api

--- auth.py
+++ auth.py
@@ -1,36 +1,46 @@
+from keras.models import load_model
+import os
+from keras.preprocessing import image
 from flask import Flask, request, Response
 import jsonpickle
 import numpy as np
 import cv2
 from pymongo import MongoClient
+from PIL import Image
 # Initialize the Flask application
 app = Flask(__name__)
+img = Image.open(
+    "image0.jpg")
 
+types = ['Compostable', 'Recyclable', 'Trash']
 
 # route http posts to this method
+
+
 @app.route('/api/login', methods=['POST'])
 def login():
     r = request.json
-    print(r)
     uri = "mongodb+srv://user:123@cluster0.uoxjp.mongodb.net/IntelliWaste?retryWrites=true&w=majority"
     client = MongoClient(uri)
     db = client.IntelliWaste
     data = db.Users.find()
+    print(data)
     found = False
-    verify=False
+    verify = False
     user = {}
     for elem in data:
+        print(elem)
         if elem['email'] == r['email']:
             found = True
-            verify = (elem['pass']==r['pass'])
+            verify = (elem['pass'] == r['pass'])
             if verify:
                 user['name'] = elem['name']
                 user['email'] = elem['email']
                 user['carbon_footprint'] = elem['carbon_footprint']
             break
-    print(found,verify)
+    print(found, verify)
 
-    return ({'found': found, 'verify':verify, 'user' : user})
+    return ({'found': found, 'verify': verify, 'user': user})
 
 
 @app.route('/api/signup', methods=['POST'])
@@ -43,17 +53,44 @@ def signup():
     data = db.Users.find()
     for elem in data:
         if elem['email'] == r['email']:
-            return ({'found':True, 'message':"Email already exists"})
+            return ({'found': True, 'message': "Email already exists"})
             break
     r['carbon_footprint'] = 0
     db.Users.insert_one(r)
-    return ({'found': False, "Message":"User added"})
+    return ({'found': False, "Message": "User added"})
+
+
+def process_image(filepath):
+    input_im = cv2.imread(filepath)
+    input_original = input_im.copy()
+    input_original = cv2.resize(
+        input_original, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+    input_im = cv2.resize(input_im, (224, 224), interpolation=cv2.INTER_LINEAR)
+    input_im = input_im / 255.
+    input_im = input_im.reshape(1, 224, 224, 3)
+    return input_im
+
+
+def get_garbage_type(preds):
+    gclass = types[np.argmax(preds)]
+    return gclass
+
+
+def predict_class(modelloc, image):
+    model = load_model(modelloc)
+    preds = model.predict(image, 1, verbose=0)
+    garb_type = get_garbage_type(preds)
+    print(preds)
+    return garb_type
+
+
+@app.route('/api/predict', methods=['POST'])
+def predict(filepath):
+    image = process_image(filepath)
+    garbage = predict_class(os.getcwd()+"/classifier/trash_mobilenet.h5")
+    return garbage
+
 
 # start flask app
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
-
-
-
-
+    app.run(host="0.0.0.0", port=3000, debug=True)
