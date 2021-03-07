06575dedd790930893dbb7ece7d7971b2e8be09c
Miki <53579064+supermikii@users.noreply.github.com>
2021-03-07 15:19:21 -0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+
+import MapScreen from './screens/mapscreen'; 
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      
+        <MapScreen></MapScreen>
+        
+    );
+  }
+}
